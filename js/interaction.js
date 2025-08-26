871fe9359b2a2ce1ad22399bc29f62ba37752011
sandera <shuang2322@gmail.com>
2025-08-26 11:40:47 -0400

    fixed homepage functionality

--- js/interaction.js
+++ js/interaction.js
@@ -1,7 +1,11 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { THREE, scene, camera, outlinePass } from './scene.js';
 import { dnaModel } from './models.js';
-import { extractKey, infos, images, defaultInfo } from './util.js';
+import { extractKey, infos, images, defaultInfo, homepage } from './util.js';
+import { setHomeStatus, smoothZoom } from './animation.js';
+
+let isOnHomePage = true;
+let homeIndex = 0;
 
 // controls
 export function setupControls(camera, renderer) {
@@ -32,7 +36,7 @@ function handleClick() {
   raycaster.setFromCamera(pointer, camera);
   const intersects = raycaster.intersectObjects(scene.children, true);
   if (intersects.length === 0){
-    document.getElementById('infoText').innerHTML = defaultInfo;
+    document.getElementById('textPart').innerHTML = defaultInfo;
     document.getElementById('infoImage').classList.add('hidden');
     outlinePass.selectedObjects = [];
   }
@@ -42,7 +46,7 @@ function handleClick() {
     obj = findTopLevelMesh(obj);
     const partName = obj.name;
     const key = extractKey(partName);
-    document.getElementById('infoText').innerHTML = infos.get(key);
+    document.getElementById('textPart').innerHTML = infos.get(key);
     document.getElementById('infoImage').classList.remove('hidden');
     document.getElementById('infoImage').innerHTML = "<img src=" + images.get(key) + ">";
     outlinePass.selectedObjects = [obj];
@@ -58,3 +62,95 @@ export function disableControls(){
   window.removeEventListener('pointermove', updatePointer);
   window.removeEventListener('click', handleClick);
 }
+
+function showHomepageUI() {
+  isOnHomePage = true;
+  homeIndex = 0;
+  zoomOut();
+  document.getElementById('textPart').innerHTML = homepage[homeIndex];
+  document.getElementById('prevBtn').classList.remove('hidden');
+  document.getElementById('prevBtn').disabled = true;
+  document.getElementById('nextBtn').disabled = false;
+  document.getElementById('nextBtn').classList.remove('hidden');
+  document.getElementById('startExploringBtn').classList.remove('hidden');
+  document.getElementById('infoImage').classList.add('hidden');
+  disableControls();
+}
+
+function showExploreUI() {
+  isOnHomePage = false;
+  enableControls();
+  document.getElementById('prevBtn').classList.add('hidden');
+  document.getElementById('nextBtn').classList.add('hidden');
+  document.getElementById('startExploringBtn').classList.add('hidden');
+  document.getElementById('infoImage').classList.remove('hidden');
+}
+
+function zoomIn() {
+  if (isOnHomePage) {
+      setHomeStatus(false);
+      enableControls();
+      showExploreUI();
+  }
+  smoothZoom(camera, { x: 0, y: 0, z: 3 }, 500);
+}
+
+function zoomOut() {
+  if (!isOnHomePage) {
+      setHomeStatus(true);
+      disableControls();
+      showHomepageUI();
+  }
+  smoothZoom(camera, { x: 0, y: 0, z: 10 }, 1000);
+}
+
+function nextPage(){
+  if(homeIndex < homepage.length - 1){
+    if(homeIndex==0){
+      document.getElementById('prevBtn').disabled = false;
+    }
+    ++homeIndex;
+    if(homeIndex==homepage.length - 1){
+      document.getElementById('nextBtn').disabled = true;
+    }
+    document.getElementById('textPart').innerHTML = homepage[homeIndex];
+  }
+}
+
+function prevPage(){
+  if(homeIndex > 0){
+    if(homeIndex==homepage.length - 1){
+      document.getElementById('nextBtn').disabled = false;
+    }
+    --homeIndex;
+    if(homeIndex==0){
+      document.getElementById('prevBtn').disabled = true;
+    }
+    document.getElementById('textPart').innerHTML = homepage[homeIndex];
+  }
+}
+
+// Attach event listeners
+// start exploring
+document.getElementById('startExploringBtn').addEventListener('click', zoomIn);
+// home
+document.getElementById('homeBtn').addEventListener('click', showHomepageUI);
+// next/previous buttons
+document.getElementById('prevBtn').addEventListener('click', prevPage);
+document.getElementById('nextBtn').addEventListener('click', nextPage);
+
+// Initial UI state
+showHomepageUI();
+
+// Toggle dropdown open/close
+const dropdown = document.getElementById('menuDropdown');
+dropdown.querySelector('.dropbtn').addEventListener('click', () => {
+  dropdown.classList.toggle('show');
+});
+
+// Close if clicking outside
+window.addEventListener('click', (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove('show');
+  }
+});
--- js/main.js
+++ js/main.js
@@ -3,12 +3,9 @@
 import '../css/style.css';
 import { scene, camera, renderer, composer, setBackground } from './scene.js' // scene, camera, renderer, composer setup
 import { getDnaModel, loadDNA } from './models.js';
-import { setupControls, enableControls, disableControls } from './interaction.js';
+import { setupControls } from './interaction.js';
 import { startAnimationLoop, setHomeStatus, smoothZoom } from './animation.js';
-import { homepage } from './util.js';
 
-let isOnHomePage = true;
-let index = 0;
 document.getElementById('prevBtn')
 
 // SCENE SETUP
@@ -23,93 +20,3 @@ const controls = setupControls(camera, renderer);
 
 // --- Animation Loop ---
 startAnimationLoop({ composer, controls });
-
-function showHomepageUI() {
-  isOnHomePage = true;
-  index = 0;
-  zoomOut();
-  document.getElementById('textPart').innerHTML = homepage[index];
-  document.getElementById('prevBtn').classList.remove('hidden');
-  document.getElementById('prevBtn').disabled = true;
-  document.getElementById('nextBtn').classList.remove('hidden');
-  document.getElementById('startExploringBtn').classList.remove('hidden');
-  document.getElementById('infoImage').classList.add('hidden');
-}
-
-function showExploreUI() {
-  isOnHomePage = false;
-  document.getElementById('prevBtn').classList.add('hidden');
-  document.getElementById('nextBtn').classList.add('hidden');
-  document.getElementById('startExploringBtn').classList.add('hidden');
-  document.getElementById('infoImage').classList.remove('hidden');
-}
-
-function zoomIn() {
-  if (isOnHomePage) {
-      setHomeStatus(false);
-      enableControls();
-      showExploreUI();
-  }
-  smoothZoom(camera, { x: 0, y: 0, z: 3 }, 500);
-}
-
-function zoomOut() {
-  if (!isOnHomePage) {
-      setHomeStatus(true);
-      disableControls();
-      showHomepageUI();
-  }
-  smoothZoom(camera, { x: 0, y: 0, z: 10 }, 1000);
-}
-
-function nextPage(){
-  if(index < homepage.length - 1){
-    if(index==0){
-      document.getElementById('prevBtn').disabled = false;
-    }
-    ++index;
-    if(index==homepage.length - 1){
-      document.getElementById('nextBtn').disabled = true;
-    }
-    document.getElementById('textPart').innerHTML = homepage[index];
-  }
-}
-
-function prevPage(){
-  if(index > 0){
-    if(index==homepage.length - 1){
-      document.getElementById('nextBtn').disabled = false;
-    }
-    --index;
-    if(index==0){
-      document.getElementById('prevBtn').disabled = true;
-    }
-    document.getElementById('textPart').innerHTML = homepage[index];
-  }
-}
-
-// Attach event listeners
-// start exploring
-document.getElementById('startExploringBtn').addEventListener('click', zoomIn);
-// home
-document.getElementById('homeBtn').addEventListener('click', showHomepageUI);
-// next/previous buttons
-document.getElementById('prevBtn').addEventListener('click', prevPage);
-document.getElementById('nextBtn').addEventListener('click', nextPage);
-
-// Initial UI state
-showHomepageUI();
-disableControls();
-
-// Toggle dropdown open/close
-const dropdown = document.getElementById('menuDropdown');
-dropdown.querySelector('.dropbtn').addEventListener('click', () => {
-  dropdown.classList.toggle('show');
-});
-
-// Close if clicking outside
-window.addEventListener('click', (e) => {
-  if (!dropdown.contains(e.target)) {
-    dropdown.classList.remove('show');
-  }
-});
